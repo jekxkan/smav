a9d6a2d4181e3e1b2655f5ab54ec132c926cf0ee
jekxkan <smirnova.eyu@mail.ru>
2025-03-16 18:12:30 +0300

    Добавлена валидация pydantic+исправлена неработающий post

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from src.router import routes
+from router import routes
 from flask import Flask
 app = Flask(__name__)
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8000)
--- src/router.py
+++ src/router.py
@@ -1,10 +1,13 @@
 import json
 import time
 
-from src.schemas import Coordinates
-from src.drone import Drone
+from pydantic import ValidationError
+
+from drone import Drone
 from dronekit import APIException
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
+from schemas import Coordinates
 
 routes = Blueprint('main', __name__)
 
@@ -12,21 +15,26 @@ routes = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-@routes.post("/post-coordinates")
-def post_coordinates(coordinates: Coordinates):
-    with open("src/coordinates.json", "a") as file:
-        json.dump(coordinates.dict(), file)
-        file.write("\n")
-    return coordinates
-
-@routes.get("/test")
-def testing():
+@routes.route("/post-coordinates", methods=['POST'])
+def post_coordinates() -> json:
+    coordinates = request.json
+    try:
+        with open("src/coordinates.json", "a") as file:
+            coordinates = Coordinates(**coordinates)
+            json.dump(dict(coordinates), file)
+            file.write("\n")
+            return {'status': 'Данные успешно отправлены'}
+    except ValidationError as e:
+        e = e.errors()[0]['loc'][0]
+        return {'status': f'Не заполнено поле {e}'}
+@routes.route("/test", methods=['GET'])
+def test() -> json:
     try:
         with open("src/coordinates.json", "r") as file:
             data = [json.loads(line) for line in file][-1]
             drone = Drone()
             drone.arm()
-            drone.takeoff(20)
+            drone.takeoff(10)
             drone.goto(**data)
             while not drone.is_arrived(**data):
                 time.sleep(3)
